6fdde0929f59c5fda3b050fe8ac9c7bfc308790e
杉山哲平 <sugiteppei@icloud.com>
2024-05-25 13:39:40 +0900

    Add files via upload

---
+++ lesson02.php
@@ -0,0 +1,14 @@
+﻿<?php
+$apple_price = 100;
+$apple_quantity = 1;
+
+$grape_price = 200;
+$grape_quantity = 3;
+
+
+$total = ($apple_price * $apple_quantity) + ($grape_price * $grape_quantity);
+
+echo "{$apple_price}円のリンゴを{$apple_quantity}個。\n";
+echo "{$grape_price}円のブドウを{$grape_quantity}個。\n";
+echo "合計は{$total}円です。";
+?>
